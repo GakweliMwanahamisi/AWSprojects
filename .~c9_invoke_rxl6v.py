ef4e9e289524322967efd042014aaecf6204e5c1
GakweliMwanahamisi <138311885+GakweliMwanahamisi@users.noreply.github.com>
2023-10-12 19:44:45 +0300

    Add files via upload

---
+++ .~c9_invoke_rxl6v.py
@@ -0,0 +1,19 @@
+print("python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+#float
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+#complex
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " +str(type(myValue)))
+#bool
+myValue=True
+print(myValue)
+print(type(myValue))
